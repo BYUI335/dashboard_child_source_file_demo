2854108e5371d529fdcdfcad44c4cd53d2504312
David Palmer <palmerda@byui.edu>
2025-01-14 16:52:22 -0700

    adding file to fork and change

---
+++ Winter2025/cannon_p.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+
+#Find the error/typo, and the fix it.
+#Save the script and use this corrected file to submit to my original repository
+#using a pull request
+
+ggplot(data = iris, 
+       aes(x = Species, 
+           y = petal.Length)) + 
+  geom_boxplot()  #ROTATE
+
