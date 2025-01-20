3cb042f284415aae57b0a27706ffbfc62378e8fa
David Palmer <palmerda@byui.edu>
2025-01-20 14:22:14 -0700

    added some notes

--- Winter2025/cannon_p.R
+++ Winter2025/cannon_p.R
@@ -10,3 +10,7 @@ ggplot(data = iris,
   geom_boxplot()  #ROTATE
 
 
+# Start a new branch for each feature development (i.e. each part of the project)
+
+# I used git branch -d david   to delete the branch locally
+# I used git remote prune origin   to remove stale branches. In other words, for me to correctly see locally what branches exist on the remote.
